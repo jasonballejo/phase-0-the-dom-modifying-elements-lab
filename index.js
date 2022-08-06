b51f3d40a31f34aafed82823c2005610fbd6b36a
Jason Ballejo <ballejo19@gmail.com>
2022-08-06 12:31:23 -0400

    commit tree ac0e04ca5cd4dfccee24e6a322857529d040826d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+document.createElement("h1").textContent = 'YOUR-NAME'
+newHeader.innerHTML = 'is the champion';
+
+document.body.append(newHeader); // appends the element
